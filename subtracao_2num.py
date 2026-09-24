4b5781c22688d6722f3aeb1461efb09884577eb1
americ4xnet <113323372+americ4xnet@users.noreply.github.com>
2026-09-24 13:37:40 -0300

    Um dois primeiros programas que fiz para estudo de lógica.

---
+++ subtracao_2num.py
@@ -0,0 +1,9 @@
+print('SUBTRAÇÃO DE DOIS NÚMEROS\n-------------------------')
+
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo valor: '))
+
+sub = num1 - num2
+
+#Limita para duas casas decimais
+print(f'\nA subtração é: {sub:.2f}\n')
