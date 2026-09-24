de9b58aaea1d04c76832748e350d7b53ac9cd96b
americ4xnet <113323372+americ4xnet@users.noreply.github.com>
2026-09-24 13:37:40 -0300

    Meu primeiro programa em Python.

---
+++ soma_2num.py
@@ -0,0 +1,13 @@
+print('SOMA DE DOIS NÚMEROS\n--------------------')
+
+#Pode-se somar números decimais
+num1 = float(input('Digite o primeiro número da soma: '))
+num2 = float(input('Digite o segundo número da soma: '))
+
+soma = num1 + num2
+
+print(f'\nO valor da soma é: {soma}\n')
+
+#Entender sobre os tipos de variáveis.
+print(f'Tipo de variável 1: {type(num1)}')
+print(f'Tipo de variável 2: {type(num2)}')
