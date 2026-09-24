090334f37f5cdb36af271f0f57fdeb4eb9c85499
americ4xnet <113323372+americ4xnet@users.noreply.github.com>
2026-09-24 14:08:22 -0300

    Multiplicação de dois inteiros.

---
+++ multiplicacao_2num.py
@@ -0,0 +1,9 @@
+print('MULTIPLICAÇÃO DE DOIS NÚMEROS INTEIROS\n'
+      '-----------------------------')
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+produto = num1 * num2
+
+print(f'\nO resultado da multiplicação é: {produto}')
