1cf086113042698baf71ed64b0fe9a2d2e2d3853
americ4xnet <113323372+americ4xnet@users.noreply.github.com>
2026-09-25 15:19:53 -0300

    Divisão de dois números.

--- divisao.py
+++ divisao.py
@@ -4,5 +4,7 @@ print('DIVISÃO DE DOIS NÚMEROS\n'
 divisor = int(input('Digite o divisor: '))
 dividendo = int(input('Digite o dividendo:  '))
 
+#result = divisor / dividendo
+
 print(f'\nO valor da divisão é: {divisor / dividendo:.2f} ')
 
